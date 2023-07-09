1c8cfefca18512fb92a0047044576e384ef46f63
OguzhanOksuz <oguzhanoksuz0@gmail.com>
2023-07-10 00:46:24 +0300

    init_mutex

--- init_philo.c
+++ init_philo.c
@@ -1,26 +1,49 @@
 #include "my_header.h"
 
+t_philo **philos_error_free(t_philo **philos, int i)
+{
+	while (i >= 0)
+		free(philos[i--]);
+	free(philos);
+	return (NULL);
+}
+
+int	init_mutex(t_rules *rules)
+{
+	int	i;
+
+	i = 0;
+	rules->forks = malloc(sizeof(pthread_mutex_t) * (rules->p_count));
+	if (!rules->forks)
+		return (0);
+	while (i < rules->p_count)
+		pthread_mutex_init(&rules->forks[i++], NULL);
+	return (1);
+}
+
 t_philo	**init_philos(t_rules *rules)
 {
 	t_philo	**philos;
 	int		i;
 
 	i = 0;
-	philos = (t_philo **)malloc(sizeof(t_philo) * (rules->p_count));
+	philos = (t_philo **)malloc(sizeof(t_philo *) * (rules->p_count));
 	if (!philos)
 		return (NULL);
 	while (i < rules->p_count)
 	{
 		philos[i] = (t_philo *)malloc(sizeof(t_philo));
 		if (!philos[i])
-		{
-		}
+			return (philos_error_free(philos, i));
 		philos[i]->id = i;
+		philos[i]->l_fork = i;
+		philos[i]->r_fork = (i + 1) % (rules->p_count);
 		philos[i]->eat_count= 0;
-		philos[i]->last_eat= 0;
+		philos[i]->last_eat = rules->start;
 		philos[i]->rules= rules;
 
 	}
+	rules->philos = philos;
 }
 
 t_rules	*init_rules(int ac, char **av)
@@ -39,11 +62,12 @@ t_rules	*init_rules(int ac, char **av)
 	else
 		rules->max_eat = -1;
 	rules->is_dead = 0;
-	rules->eat_limit = 0;
+	rules->eaten = 0;
+	rules->start = get_time();
 	init_philos(rules);
 	if (!rules->philos)
 		return (free_rules(rules), NULL);	
-	init_mutex(rules);
+	rules->mutex = init_mutex(rules);
 	if (!rules->mutex)
 		return (free_rules(rules), free_philos(rules->philos), NULL);	
 	return (rules);
--- my_header.h
+++ my_header.h
@@ -10,21 +10,26 @@ void	philo(int ac, char **av);
 typedef struct	s_philo
 {
 	int				id;
+	int				l_fork;
+	int				r_fork;
 	int				eat_count;
 	long long		last_eat;
-	struct s_rules	*rules;
-}					t_philo;
+	struct s_rules		*rules;
+}				t_philo;
 
 typedef struct s_rules
 {
-	int		p_count;
-	int		death_t;
-	int		eat_t;
-	int		sleep_t;
-	int		max_eat;
-	int		is_dead;
-	int		eat_limit;
-	t_phlo	**philos;
+	int			p_count;
+	int			death_t;
+	int			eat_t;
+	int			sleep_t;
+	int			max_eat;
+	int			is_dead;
+	int			eaten;
+	int			mutux
+	long long	start;
+	t_phlo		**philos;
+	pthread_mutex_t	*forks;
 }			t_rules;
 
 #endif
